61b74d49fe2e9149275b25a00c3b95e5dd8812a4
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2023-08-08 07:20:08 -0300

    product

--- src/main/java/com/projetospring/curso/entities/Product.java
+++ src/main/java/com/projetospring/curso/entities/Product.java
@@ -13,27 +13,33 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
-	
+@Table(name = "tb_product")
+public class Product implements Serializable{
+		
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String desciption;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
-		
+	public Product() {		
+	
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String desciption, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.desciption = desciption;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -51,9 +57,33 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getDesciption() {
+		return desciption;
+	}
+
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
 	
-	public void setProducts(Set<Product> products) {
-		this.products = products;
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -69,13 +99,14 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
 	@Override
 	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
+		return "Product [id=" + id + ", name=" + name + ", desciption=" + desciption + ", price=" + price + ", imgUrl="
+				+ imgUrl + "]";
 	}
-
+		
 }
